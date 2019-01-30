f3bf8b06820067f4a924599b35c116284c695cf1
echoman12 <45900323+echoman12@users.noreply.github.com>
2019-01-30 05:14:44 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ const prefix = '-'
 
 	
     var ms = 60000 ;
-    var setGame = [`${client.guilds.size} Server`,' Premium Bot by n3k4a & Baron','Type -help |-support |-invite ',`${client.users.size} Members`,'-invite','By: n3k4a & Hosam | BaronTube'];
+    var setGame = [`${client.guilds.size} Server`,' by waled','Type -help ',`${client.users.size} Members`,'By: waled'];
     var i = -1;
     var j = 0;
     setInterval(function (){
